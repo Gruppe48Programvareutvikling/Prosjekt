2dd06a862fec3676763caa03daa694077accc277
Hansch <hans_c40@hotmail.com>
2015-02-24 10:56:05 +0100

    d

--- TDT4140 Programvareutvikling/src/Arbeider/klasse1.java
+++ TDT4140 Programvareutvikling/src/Arbeider/klasse1.java
@@ -5,14 +5,16 @@ public class klasse1 {
 	
 	public static void main(String[] args) {
 		int tall = 1;
+		string h = "Halla gutta"
 		double cunt = 6;
 		for (int i = 0; i < cunt; i++) {
 			System.out.println(cunt);
 		}
 		
 	}
+
 	
 	
->>>>>>> branch 'HEAD' of https://github.com/Gruppe48Programvareutvikling/Prosjekt.git
+
 
 }
